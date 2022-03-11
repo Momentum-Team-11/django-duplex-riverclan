557963ba3cb8648fce4dcddb5e07ac876ddd36fd
JDitto95 <powell0295@gmail.com>
2022-03-11 14:59:01 -0500

    created home, added url to webpage

--- RiverClan/settings.py
+++ RiverClan/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'RiverClan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
