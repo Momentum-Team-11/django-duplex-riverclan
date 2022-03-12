0a0d2402f833844bad37339a3479dd720dfae5b8
JDitto95 <powell0295@gmail.com>
2022-03-12 10:11:58 -0500

    made templates file, edited settings

--- flash_cards/views.py
+++ flash_cards/views.py
@@ -3,4 +3,5 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 # Create your views here.
 
 def index(request):
+    
     return render(request, "flash_cards/homepage.html")
