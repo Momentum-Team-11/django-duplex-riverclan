7e107cacf975d7551ea4e7b37679bb8dfe9118d6
Adam Lindgren <98040659+ChariotOfFLAME@users.noreply.github.com>
2022-03-12 16:39:36 -0500

    Update RiverClan/settings.py

--- RiverClan/settings.py
+++ RiverClan/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'RiverClan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        "DIRS": [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
