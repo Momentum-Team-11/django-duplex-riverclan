773920222510f04ddb46ada7486efbf7b0149cba
JDitto95 <powell0295@gmail.com>
2022-03-15 10:03:55 -0400

    edtd-list-func

--- flash_cards/views.py
+++ flash_cards/views.py
@@ -5,11 +5,15 @@ from flash_cards.models import FlashCard
 # Create your views here.
 
 def list_cards(request, pk):
-    cards = FlashCard.objects.get(deck=pk)
+    
+    cards = FlashCard.objects.all(deck=pk)
     sort_by = request.GET.get("sort")
     return render( 
         request, "FlashCard/list_cards.html", {"cards": cards, "sort_by": sort_by} )
     
+    
+    #def add_card(request, pk):
+        
 def index(request):
     
     return render(request, "flash_cards/homepage.html")
