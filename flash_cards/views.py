9a434846cd8135e40c52aa2a9bc7077bea475b6b
JDitto95 <powell0295@gmail.com>
2022-03-15 01:41:44 -0400

    added the list_cards

--- flash_cards/views.py
+++ flash_cards/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, user_passes_test 
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+from flash_cards.models import FlashCard 
 # Create your views here.
 
+def list_cards(request, pk):
+    cards = FlashCard.objects.get(deck=pk)
+    sort_by = request.GET.get("sort")
+    return render( 
+        request, "FlashCard/list_cards.html", {"cards": cards, "sort_by": sort_by} )
+    
 def index(request):
     
     return render(request, "flash_cards/homepage.html")
