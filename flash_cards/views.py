a7981592bac91465feef1b3d84c34aa06bb7367c
JDitto95 <powell0295@gmail.com>
2022-03-15 20:09:26 -0400

    list_decks,views.py, models.py

--- flash_cards/views.py
+++ flash_cards/views.py
@@ -8,14 +8,14 @@ def list_decks(request):
     decks = Deck.objects.all()
     sort_by = request.GET.get("sort") or "title"
     return render( 
-        request, "FlashCard/list_decks.html", {"decks": decks, "sort_by": sort_by} )
+        request, "flash_cards/list_decks.html", {"decks": decks, "sort_by": sort_by} )
     
     
 def list_cards(request, pk):
         cards = FlashCard.objects.all(deck=pk)
         sort_by = request.GET.get("sort") or "title"
         return render(
-            request, "FlashCard/list_cards.html", {"cards": cards, "sort_by": sort_by}
+            request, "flash_cards/list_cards.html", {"cards": cards, "sort_by": sort_by}
         )
         
 def index(request):
